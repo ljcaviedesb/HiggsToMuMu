c4b7ea8c2584d385dddbf33c638b4374e482c772
Laura Juliana Caviedes Betancourt <lcaviede@cern.ch>
2021-05-10 09:35:12 -0500

    Multiple Higgs decay

--- mymain01.cc
+++ mymain01.cc
@@ -41,10 +41,22 @@ int main (int argc, char* arg[]){
     if(!pythia.next()) continue;
 
     for (int i = 0; i<pythia.event.size(); ++i){
-     if (pythia.event[i].id()==13 || pythia.event[i].id()==-13 || pythia.event[i].id()==25){
-    	imu = i;
-	imum = i;
-	iHiggs= i;
+      Int_t muon = pythia.event[i].id()==13;
+      Int_t muonm = pythia.event[i].id()==-13;
+      Int_t bottom = pythia.event[i].id()==5;
+      Int_t top = pythia.event[i].id()==6;
+      Int_t charm = pythia.event[i].id()==4;
+      Int_t gluon = pythia.event[i].id()==21;
+      Int_t foton = pythia.event[i].id()==22;
+      Int_t Z = pythia.event[i].id()==23;
+      Int_t W = pythia.event[i].id()==24;
+      Int_t H = pythia.event[i].id()==25;
+
+      /*To make every ROOT file for each Higgs decay change the if for each decay wanted 
+	like if (bottom || H){ or if (Z || H){  and also is needed to be change the name 
+	of the application and output root file*/
+      if (muon || H){
+
 	pT = pythia.event[i].pT();
 	px = pythia.event[i].px();
 	py = pythia.event[i].py();
